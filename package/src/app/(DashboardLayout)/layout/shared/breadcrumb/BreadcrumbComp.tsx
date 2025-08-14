c1eb9e23fe0a5661125efc5211130d762f31a449
Tailwind-Admin <tailwindadmiin@gmail.com>
2025-08-14 18:33:23 +0530

    Update BreadcrumbComp.tsx

--- package/src/app/(DashboardLayout)/layout/shared/breadcrumb/BreadcrumbComp.tsx
+++ package/src/app/(DashboardLayout)/layout/shared/breadcrumb/BreadcrumbComp.tsx
@@ -16,7 +16,7 @@ interface BreadCrumbType {
 const BreadcrumbComp = ({ items, title }: BreadCrumbType) => {
   return (
     <>
-      <CardBox className={`mb-6 py-4 bg-lightinfo dark:bg-darkinfo overflow-hidden rounded-md border-none !shadow-none dark:!shadow-none`}>
+      <CardBox className={`mb-6 py-4 bg-lightsecondary dark:bg-darkinfo overflow-hidden rounded-md border-none !shadow-none dark:!shadow-none`}>
         <div className=" items-center grid grid-cols-12 gap-6">
             <div className="col-span-9">
                 <h4 className="font-semibold text-xl text-dark dark:text-white mb-3">{title}</h4>
